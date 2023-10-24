fd9ccde563a79f5cde1c45becf4396836a44d96b
Andre <syahandrean59@gmail.com>
2023-10-24 10:10:46 +0700

    pertama

---
+++ screens/index.js
@@ -0,0 +1,29 @@
+import LotsOfStyles from "./LotsOffStyles";
+import FixedDimensionsBasics from "./FixedDimensionsBasics";
+import FlexDimensionsBasics from "./FlexDimensionsBasics";
+import PercentageDimensionsBasics from "./PercentageDimensionBasics";
+import FlexBasic from "./FlexBasic";
+import FlexDirectionBasics from "./FlexDirectionBasics";
+import JustifyContentBasics from "./JustifyContentBasics";
+import AlignItemsLayout from "./AlignItemsLayout";
+import WidthHeightBasics from "./WidthHeightBasics";
+import PositionLayout from "./PositionLayout";
+import DisplayAnImageWithStyle from "./DisplayAnImageWithStyle";
+import NativeBaseBasic from "./NativeBaseBasic";
+
+export {
+  LotsOfStyles,
+  FixedDimensionsBasics,
+  FlexDimensionsBasics,
+  PercentageDimensionsBasics,
+  FlexBasic,
+  FlexDirectionBasics,
+  JustifyContentBasics,
+  AlignItemsLayout,
+  WidthHeightBasics,
+  PositionLayout,
+  DisplayAnImageWithStyle,
+  NativeBaseBasic,
+};
+
+
